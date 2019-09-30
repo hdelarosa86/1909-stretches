d04820fc391980358314466c8654324ad70e4580
Hector DelaRosa <hdelaros@gmail.com>
2019-09-30 19:05:25 -0400

    largestDivisible solution

--- questions/01_largestDivisible/largestDivisible.js
+++ questions/01_largestDivisible/largestDivisible.js
@@ -4,7 +4,12 @@ write the largestDivisible function
 
 */
 
-function largestDivisible(arr, num) {}
+function largestDivisible(arr, num) {
+    arr = arr.filter( item => {
+        return item % num === 0; 
+    })
+    return (arr.length > 0) ? Math.max(...arr) : null;
+}
 
 // console.log(largestDivisible([1, 2, 3, 4, 5, 6, 7], 3));//6
 // console.log(largestDivisible([1], 2));//null
