f3fcd86985ef457b2f561cc925811e0f52762b69
Hector DelaRosa <hdelaros@gmail.com>
2019-09-30 19:08:37 -0400

    isEqual solution

--- questions/00_isEqual/isEqual.js
+++ questions/00_isEqual/isEqual.js
@@ -3,6 +3,16 @@ function isEqual(obj1, obj2) {
   // run your tests by running `jest isEqual`
   // you can also run tests on every save by running
   // `jest --watch isEqual`
+  const obj1ValueArr = Object.values(obj1);
+  const obj2ValueArr = Object.values(obj2);
+  const obj1KeysArr = Object.keys(obj1);
+  const obj2KeysArr = Object.keys(obj2);
+
+  if(obj1ValueArr.length === obj2ValueArr.length && obj1KeysArr.length === obj2KeysArr.length ) {
+    return obj1ValueArr.join('') === obj2ValueArr.join('') && obj1KeysArr.join('') === obj2KeysArr.join('')
+  } else{
+    return false;
+  }
 }
 
 module.exports = { isEqual };
