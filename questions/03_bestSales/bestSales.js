d36998e44ea82ec6edbb00b7d73aa1aa7ad51e91
Hector DelaRosa <hdelaros@gmail.com>
2019-10-09 13:10:04 -0400

    bestSales solution

--- questions/03_bestSales/bestSales.js
+++ questions/03_bestSales/bestSales.js
@@ -26,11 +26,29 @@ productWhichMadeMostMoney returns an object with an id for that product and a to
 
 //write the productWhichMadeMostMoney function
 
-function productWhichMadeMostMoney(arr){
-
-}
-
 function bestSales(sales) {
+  let obj = sales.reduce( (obj, product) => {
+    obj[product['productId']] ? 
+    obj[product['productId']] += product['amount'] : 
+    obj[product['productId']] = product['amount'] ;
+    return obj;
+  },{})
+let bestSeller = {};
+if(sales.length !== 0){
+ obj = Object.entries(obj)
+ obj.reduce( (initialVal, currVal) =>{
+   if(initialVal[1] < currVal[1]){
+     initialVal = currVal;
+   }
+   bestSeller.id = parseInt(initialVal[0]);
+   bestSeller.total = initialVal[1];
+   return initialVal;
+ })
 }
+
+ return bestSeller;
+}
+
+console.log(bestSales(sales));
 
 module.exports = { bestSales, sales };
