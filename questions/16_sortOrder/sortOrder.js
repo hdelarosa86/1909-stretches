90efc4e1a9eac2cf8df387c73d589b884d92d780
Hector DelaRosa <hdelaros@gmail.com>
2019-11-20 18:41:13 -0500

    updated

--- questions/16_sortOrder/sortOrder.js
+++ questions/16_sortOrder/sortOrder.js
@@ -4,17 +4,31 @@ and a key for the return value.
 */
 
 function sortOrder(data, order, key) {
-   let sortedData = data.map( item => {
-        return item[order];
+  //    let sortedData = data.map( item => {
+  //         return item[order];
+  //     });
+  //    sortedData.sort((a,b) => a-b)
+  //    return data.map( item => {
+  //        for(let i = 0; i < sortedData.length; i++){
+  //            if(sortedData[i] === item[order]){
+  //                return data[key];
+  //            }
+  //        }
+  //    })
+
+  return data
+    .sort((a, b) => {
+      if (a[order] < b[order]) {
+        return -1;
+      }
+      if (a[order] > b[order]) {
+        return 1;
+      }
+      return 0;
+    })
+    .map(ele => {
+      return ele[key];
     });
-   sortedData.sort((a,b) => a-b)
-   return data.map( item => {
-       for(let i = 0; i < sortedData.length; i++){
-           if(sortedData[i] === item[order]){
-               return data[key];
-           }
-       }
-   })
 }
 
 module.exports = { sortOrder };
